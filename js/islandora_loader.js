449b0743cc994906eb618053cc8a57b42d181ff1
Alan Stanley <astanley@upei.ca>
2012-03-15 10:29:51 -0300

    Initial Islandora Commit

---
+++ js/islandora_loader.js
@@ -0,0 +1,28 @@
+// Created Wed Mar  7 09:19:09 2012
+// This file is generated dynamically during the Drupal installation process.
+// Any changes made to this file will be lost on reinstallation.
+// Clone and rename this file if changes are to survive module reactivation.
+
+    $.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (!results)
+  {
+    return 0;
+  }
+  return results[1] || 0;
+}
+
+  PID = $.urlParam('pid');
+
+  $.ajax({
+    url:'http://localhost/Development/bookreader/setup/' + PID,
+    async:false,
+    success: function(data, status, xhr) {
+      islandora_params = data;
+    },
+    error: function() {
+      alert("AJAX call failed");
+    },
+    dataType: 'json'
+  });
+
