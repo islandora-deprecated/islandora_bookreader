4e1db019773483fe1ab63c1fd8d95d9169889369
Paul Pound <ppound@upei.ca>
2012-04-30 12:49:13 -0300

    initial changes for text highlighting

--- mainpage.php
+++ mainpage.php
@@ -209,7 +209,7 @@
       // read-aloud and search need backend compenents and are not supported in the demo
       $('#BRtoolbar').find('.read').hide();
       //$('#textSrch').hide();
-      $('#btnSrch').hide();
+      //$('#btnSrch').hide();
 
     </script>
 
